0bf0f844b2a96c60f685ba935a601d1994c7b797
udhai-kumar <udaisaravana@gmail.com>
2024-04-07 20:00:11 +0530

    Footer fixed in bottom and scrolltotop enabled in route change

--- src/routes/Routers.js
+++ src/routes/Routers.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import {Routes,Route} from 'react-router-dom'
+import React, { useEffect } from 'react'
+import {Routes,Route,useLocation} from 'react-router-dom'
 import Home from '../pages/Home'
 import Shop from '../pages/Shop'
 import Cart from '../pages/Cart'
@@ -7,6 +7,11 @@ import About from '../pages/About'
 import ProductDetails from '../pages/ProductDetails'
 
  function Routers() {
+  const location = useLocation();
+  useEffect(()=>{
+    window.scrollTo(0, 0); // Scroll to top when route changes
+
+  },[location.pathname])
   return <>
   <Routes>
   <Route path='/' element ={<Home/>}/>
